db376521cf7d36dcd261e61ce424fc6ca56b5db8
mohammadmazrouei <mohammademazrouei@gmail.com>
2024-07-19 07:58:52 +0330

    add more exception handeling to Sparse_Table

--- Data_Structures/Sparse_Table.cpp
+++ Data_Structures/Sparse_Table.cpp
@@ -35,7 +35,7 @@ struct SparseTable {
         }
     }
     T get(int l, int r) { 
-        assert(l >= 0 && r < n);
+        assert(l >= 0 && l < n && r >= 0 && r < n);
         //int k = __builtin_clzll(1) - __builtin_clzll(r - l + 1);
         int k = __builtin_clz(1) - __builtin_clz(r - l + 1);
         return f(t[l][k], t[r - (1 << k) + 1][k]);
@@ -56,7 +56,6 @@ void solve() {
         int l, r;
         cin >> l >> r;
         l--, r--;
-
         cout << st.get(l, r) << '\n';
     }
 }
