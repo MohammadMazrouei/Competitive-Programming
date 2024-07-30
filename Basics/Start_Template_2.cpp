b4b6be6cda7c1474729126dad8b068396fcfd0d4
mohammadmazrouei <mohammademazrouei@gmail.com>
2024-07-30 11:15:16 +0330

    create Start_Template_2

---
+++ Basics/Start_Template_2.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+        
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
