85481580c1ce9389bb5f214560a299b367f8baed
mohammadmazrouei <mohammademazrouei@gmail.com>
2024-08-06 17:29:02 +0330

    Create Divisors

---
+++ Basics/Divisors.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Find Divisors, O(sqrt(n))
+vector<int64_t> divisors(int64_t n) {
+    vector<int64_t> d;
+    for (int64_t i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            d.push_back(i);
+            if (i * i != n) {
+                d.push_back(n / i);
+            }
+        }
+    }
+    return d;
+}
+
+void solve() {
+    int64_t n;
+    cin >> n;
+
+    vector<int64_t> div = divisors(n);
+    sort(div.begin(), div.end());
+    for (auto d : div) {
+        cout << d << " \n"[d == div.back()];
+    }
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    //cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
