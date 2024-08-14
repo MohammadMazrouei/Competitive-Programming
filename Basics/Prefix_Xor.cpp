2d706fa2439aebd15392eeafcb443ca6271824ac
mohammadmazrouei <mohammademazrouei@gmail.com>
2024-08-14 10:40:55 +0330

    Create Prefix_Xor

---
+++ Basics/Prefix_Xor.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n, q;
+    cin >> n >> q;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    vector<int64_t> p(n + 1);
+    for (int i = 1; i <= n; i++) {
+        p[i] = p[i - 1] ^ a[i - 1];
+    }
+
+    while (q--) {
+        int l, r;
+        cin >> l >> r;
+        l--;
+
+        int64_t ans = p[r] ^ p[l];
+        cout << ans << '\n';
+    }
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    //cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
