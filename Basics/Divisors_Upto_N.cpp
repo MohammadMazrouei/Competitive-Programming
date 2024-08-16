b9c3bdb7ca271a428da3529e0887b34407a220a5
mohammadmazrouei <mohammademazrouei@gmail.com>
2024-08-16 12:41:01 +0330

    Update Divisors_Upto_N

--- Basics/Divisors_Upto_N.cpp
+++ Basics/Divisors_Upto_N.cpp
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Find All Divisors Upto n, O(n*log(n))
 void solve() {
     int n;
     cin >> n;
 
+    // Find All Divisors Upto n, O(n*log(n))
     vector<vector<int>> div(n + 1);
     for (int i = 1; i <= n; i++) {
         for (int j = i; j <= n; j += i) {
