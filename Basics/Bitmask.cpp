694a42306dfc844519bde9b52fb52297b71b5fa9
mohammadmazrouei <mohammademazrouei@gmail.com>
2024-08-16 18:23:04 +0330

    Create Bitmask

---
+++ Basics/Bitmask.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    // Calculate sum of every subset, O(2^n * n)
+    vector<int64_t> sums;
+    for (int mask = 0; mask < (1 << n); mask++) {
+        int64_t sum = 0;
+        for (int i = 0; i < n; i++) {
+            if (mask >> i & 1) {
+                sum += a[i];
+            }
+        }
+        sums.push_back(sum);
+    }
+
+    for (auto x : sums) {
+        cout << x << " \n"[sums.back() == x];
+    }
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    //cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
