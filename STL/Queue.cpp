90a65448aa526390b3a8e2020c8039fc3faae2cd
mohammadmazrouei <mohammademazrouei@gmail.com>
2024-08-17 15:01:22 +0330

    Create Queue

---
+++ STL/Queue.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    queue<int> q;    
+
+    // push, emplace, pop, front, back
+    // size, empty
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    //cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
