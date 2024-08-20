41ba37c6989c33a428adbcea32a569dfda851f75
mohammadmazrouei <mohammademazrouei@gmail.com>
2024-08-20 16:29:22 +0330

    Create Difference_Array

---
+++ Basics/Difference_Array.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n, q;
+    cin >> n >> q;
+
+    vector<int64_t> a(n + 1);
+    while (q--) {
+        int l, r, x;
+        cin >> l >> r >> x;
+        l--;
+        a[l] += x;
+        a[r] -= x;
+    }
+    for (int i = 1; i < n; i++) {
+        a[i] += a[i - 1];
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " \n"[i == n - 1];
+    }
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    //cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
