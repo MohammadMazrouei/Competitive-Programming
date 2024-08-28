1b6f31eb59adf38672d36a46757a81e97113ce3d
mohammadmazrouei <mohammademazrouei@gmail.com>
2024-08-28 12:07:50 +0330

    Create Priority_Queue

---
+++ STL/Priority_Queue.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    priority_queue<int> pqg;    
+    priority_queue<int, vector<int>, greater<int>> pql;    
+
+    // push, emplace, pop, top
+    // size, empty
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    //cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
