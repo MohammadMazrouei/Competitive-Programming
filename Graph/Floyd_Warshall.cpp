e7b56db156ac061a4c1ec1214e17ccffcdefa3c3
mohammadmazrouei <mohammademazrouei@gmail.com>
2024-09-12 18:39:59 +0330

    Update Floyd Warshall

--- Graph/Floyd_Warshall.cpp
+++ Graph/Floyd_Warshall.cpp
@@ -10,12 +10,12 @@ void solve() {
     // if graph have negative weight cycle, exist a vertex v such that dist[v][v] < 0
     constexpr int64_t INF = 1e18;
     vector dist(n, vector<int64_t>(n, INF));
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < m; i++) {
         int u, v, w;
         cin >> u >> v >> w;
         u--, v--;
-        dist[u][v] = w;
-        dist[v][u] = w;
+        dist[u][v] = min<int64_t>(dist[u][v], w);
+        dist[v][u] = min<int64_t>(dist[v][u], w);
     }
     for (int i = 0; i < n; i++) {
         dist[i][i] = 0;
