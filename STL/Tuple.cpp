9503990548151211cfa735ac78a0153e19020968
mohammadmazrouei <mohammademazrouei@gmail.com>
2024-09-13 11:13:10 +0330

    Update Tuple

--- STL/Tuple.cpp
+++ STL/Tuple.cpp
@@ -2,9 +2,7 @@
 using namespace std;
 
 void solve() {
-    tuple<int, int, int, string> t(10, 20, 30, "t1");    
-    t = make_tuple(11, 21, 31, "t2");
-    t = {12, 22, 32, "t3"};
+    tuple<int, int, int, string> t(10, 20, 30, "t1"), t2 = make_tuple(11, 21, 31, "t2"), t3 = {12, 22, 32, "t3"};
     get<3>(t) = "hi";
 
     // auto new_tuple = tuple_cat(t1, t2), Concat t1 & t2 in new_tuple
