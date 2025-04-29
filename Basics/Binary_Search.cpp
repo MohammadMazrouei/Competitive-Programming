1e17ac581a29115c47eefc37746ca99f1bfbc4e1
mohammadmazrouei <mohammademazrouei@gmail.com>
2025-04-29 18:52:26 +0330

    Update Binary Search

--- Basics/Binary_Search.cpp
+++ Basics/Binary_Search.cpp
@@ -5,23 +5,8 @@ void solve() {
     int n;
     cin >> n;
 
-    // Lowest x Satisfy f(x), O(log(n))
+    // Highest x satisfy f(x), O(log(n))
     auto binary_search1 = [&](int low, int high, const function<bool(const int)> &f) -> int {
-        int lo = low - 1, hi = high + 1;
-        while (hi - lo > 1) {
-            int mid = lo + (hi - lo) / 2;
-            if (f(mid)) {
-                hi = mid;
-            }
-            else {
-                lo = mid;
-            }
-        }
-        return hi;
-    };
-
-    // Highest x Satisfy f(x), O(log(n))
-    auto binary_search2 = [&](int low, int high, const function<bool(const int)> &f) -> int {
         int lo = low - 1, hi = high + 1;
         while (hi - lo > 1) {
             int mid = lo + (hi - lo) / 2;
@@ -35,7 +20,22 @@ void solve() {
         return lo;
     };
 
-    // Real Binary Search, O(log(n / eps))
+    // Lowest x satisfy f(x), O(log(n))
+    auto binary_search2 = [&](int low, int high, const function<bool(const int)> &f) -> int {
+        int lo = low - 1, hi = high + 1;
+        while (hi - lo > 1) {
+            int mid = lo + (hi - lo) / 2;
+            if (f(mid)) {
+                hi = mid;
+            }
+            else {
+                lo = mid;
+            }
+        }
+        return hi;
+    };
+
+    // Real value binary search, O(log(n / eps))
     const double eps = 1e-9;
     auto binary_search = [&](double low, double high, const function<bool(const double)> &f) -> double {
         double lo = low, hi = high;
@@ -51,9 +51,9 @@ void solve() {
         return lo;
     };
 
-    // Calculate ceil_sqrt & floor_sqrt
-    cout << binary_search1(0, n, [&](int x) { return (int64_t)x * x >= n; }) << '\n';
-    cout << binary_search2(0, n, [&](int x) { return (int64_t)x * x <= n; }) << '\n';
+    // Calculate floor sqrt & ceil sqrt
+    cout << binary_search1(0, n, [&](int x) { return (int64_t)x * x <= n; }) << '\n';
+    cout << binary_search2(0, n, [&](int x) { return (int64_t)x * x >= n; }) << '\n';
 
     cout << fixed << setprecision(9);
     cout << binary_search(0, n, [&](double x) { return x * x <= n; }) << '\n';
