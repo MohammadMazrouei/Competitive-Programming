de3467412f2a2aa196db4b62c1c58209afd8d5e6
mohammadmazrouei <mohammademazrouei@gmail.com>
2025-04-29 16:15:57 +0330

    Fix Bug in Counting Sort

--- Basics/Counting_Sort.cpp
+++ Basics/Counting_Sort.cpp
@@ -4,8 +4,8 @@ using namespace std;
 // Counting Sort, Stable, O(n + M)
 template <typename T>
 void counting_sort(vector<T> &a) {
-    static_assert(is_integral_v<T> && is_signed_v<T>,
-                  "counting_sort requires signed integral type");
+    static_assert(is_integral_v<T>,
+                  "counting_sort requires integral type");
 
     int n = a.size();
     int M = *max_element(a.begin(), a.end());
