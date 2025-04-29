5092bab54bd172e09b0e4aa4247dc953e6c6470d
mohammadmazrouei <mohammademazrouei@gmail.com>
2025-04-29 14:12:10 +0330

    Update GCD and LCM

--- Basics/GCD_and_LCM.cpp
+++ Basics/GCD_and_LCM.cpp
@@ -2,8 +2,10 @@
 using namespace std;
 
 // Greatest Common Divisor, O(log(min(a, b)))
+// gcd always be non-negative, gcd(*, 0) = 0
 template <typename T>
-T _gcd(T a, T b) {
+T my_gcd(T a, T b) {
+    a = abs(a), b = abs(b);
     while (b) {
         a %= b;
         swap(a, b);
@@ -12,17 +14,22 @@ T _gcd(T a, T b) {
 }
 
 // Least Common Multiple, O(log(min(a, b)))
+// lcm always be non-negative, lcm(*, 0) = 0
 template <typename T>
-T _lcm(T a, T b) {
-    return a / _gcd(a, b) * b;
+T my_lcm(T a, T b) {
+    if (a == 0 || b == 0) {
+        return 0;
+    }
+    a = abs(a), b = abs(b);
+    return a / my_gcd(a, b) * b;
 }
 
 void solve() {
     int64_t a, b;
     cin >> a >> b;
 
-    int64_t g1 = _gcd(a, b), l1 = _lcm(a, b);
-    int64_t g2 = gcd(a, b), l2 = lcm(a, b);
+    int64_t g1 = gcd(a, b), l1 = lcm(a, b);
+    int64_t g2 = my_gcd(a, b), l2 = my_lcm(a, b);
 
     cout << g1 << ' ' << l1 << '\n';
     cout << g2 << ' ' << l2 << '\n';
