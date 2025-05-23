6f005003f3b609221abae8fda90a23483815963c
mohammadmazrouei <mohammademazrouei@gmail.com>
2025-05-23 13:41:23 +0330

    Update Pair

--- STL/Pair.cpp
+++ STL/Pair.cpp
@@ -2,14 +2,17 @@
 using namespace std;
 
 void solve() {
+    // --- Initialization ---
     pair<int, string> p1(1, "p1"), 
                       p2 = {2, "p2"},
                       p3 = make_pair(3, "p3");
 
+    // --- Unpack elements ---
     int a1; string s1;
     tie(a1, s1) = p1;
     auto [a2, s2] = p2;
 
+    // --- Element access ---
     cout << p1.first << ' ' << p1.second << '\n';
     cout << get<0>(p1) << ' ' << get<1>(p1) << '\n';
 }
