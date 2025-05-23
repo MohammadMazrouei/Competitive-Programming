e145d243838bc4e62796f020b463fa74eae80e8e
mohammadmazrouei <mohammademazrouei@gmail.com>
2025-05-23 11:36:29 +0330

    Update Tuple

--- STL/Tuple.cpp
+++ STL/Tuple.cpp
@@ -2,16 +2,18 @@
 using namespace std;
 
 void solve() {
-    tuple<int, int, int, string> t(10, 20, 30, "t1"), t2 = make_tuple(11, 21, 31, "t2"), t3 = {12, 22, 32, "t3"};
-    get<3>(t) = "hi";
+    tuple<int, int, string> t1(1, 10, "t1"), 
+                            t2 = {2, 20, "t2"},
+                            t3 = make_tuple(3, 30, "t3");
 
-    // auto new_tuple = tuple_cat(t1, t2), Concat t1 & t2 in new_tuple
+    int a1, b1; string s1;
+    tie(a1, b1, s1) = t1;
+    auto [a2, b2, s2] = t2;
 
-    int a, b, c; string s;
-    tie(a, b, c, s) = t;
-    auto [a2, b2, c2, s2] = t;
+    // concatenated_tuple is now: tuple<int, int, string, int, int, string>
+    auto concatenated_tuple = tuple_cat(t1, t2); 
 
-    cout << get<0>(t) << ' ' << get<1>(t) << ' ' << get<2>(t) << ' ' << get<string>(t) << '\n';
+    cout << get<0>(t1) << ' ' << get<1>(t1) << ' ' << get<string>(t1) << '\n';
 }
 
 int32_t main() {
